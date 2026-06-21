c47f96d72488bd7ebc66416c6df7823ed51e8e3e
Aathi <aathithirunav@gmail.com>
2026-06-21 20:19:35 +0530

    Add hierarchicalInheritance class to demonstrate hierarchical inheritance in Java

---
+++ OOPs/inheritance/hierarchicalInheritance.java
@@ -0,0 +1,29 @@
+package JavaBootcamp.OOPs.Inheritance;
+
+class Animal{
+    void sound(){
+        System.out.println("uuuuuu");
+    }
+}
+
+class dog extends Animal{
+    void sound(){
+        System.out.println("bark");
+    }    
+}
+class cat extends Animal{
+    void sound(){
+        System.out.println("meow");
+    } 
+}
+
+public class hierarchicalInheritance {
+
+    public static void main(String[] args) {
+
+        Animal c = new cat();
+        Animal d = new dog();
+        c.sound();
+        d.sound();
+    }
+}
