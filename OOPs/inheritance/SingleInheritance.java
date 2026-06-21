ae16d51f206e43efcdadd2268bbf33b45870e58e
Aathi <aathithirunav@gmail.com>
2026-06-21 20:19:53 +0530

    Add SingleInheritance class to demonstrate single inheritance in Java

---
+++ OOPs/inheritance/SingleInheritance.java
@@ -0,0 +1,39 @@
+package JavaBootcamp.OOPs.Inheritance;
+
+class Parent{
+    int age;
+    String name = "dad";
+
+    Parent(String gpname, int age){
+        this.age = age;
+        
+    }
+
+    void sound(){
+        System.out.println("ahhhhyyyyyyyyy");
+    }
+    
+}
+class Son extends Parent{
+    String name;
+
+    Son(String gpname, int age, String name){
+        super(gpname,age);
+        this.name = name;
+    }
+
+    void sound(){
+        System.out.println("ahhhhyyyyyyyyy");
+    }
+}
+
+public class SingleInheritance {
+    public static void main(String[] args) {
+        // Son son = new Son("gp", 50, "son");
+        // System.out.println(son.name);
+
+        Parent parent = new Son("gp", 40, "p");
+        System.out.println(parent.name);
+        parent.sound();   
+    }
+}
