1e902840f4ed28c1e10b00c9125300466a09b148
Aathi <aathithirunav@gmail.com>
2026-06-22 20:08:20 +0530

    add Main.java file, create instance for singleton class

---
+++ design-pattern-singleton-class/Main.java
@@ -0,0 +1,10 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        Singleton s = Singleton.getInstance();
+        Singleton s1 = Singleton.getInstance();
+        if(s == s1) System.out.println("tere");
+
+    }
+}
