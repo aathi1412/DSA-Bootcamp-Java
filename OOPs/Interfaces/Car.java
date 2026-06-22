2cd9f87b099179d1ec3172e7cec868b9c2bc545d
Aathi <aathithirunav@gmail.com>
2026-06-22 21:06:44 +0530

    add Car class implementing Engine, InteriorDesign, and MediaPlayer interfaces

---
+++ OOPs/Interfaces/Car.java
@@ -0,0 +1,45 @@
+package JavaBootcamp.OOPs.Interfaces;
+
+public class Car implements Engine, InteriorDesign, MediaPlayer{
+
+    @Override
+    public void playMusic() {
+        System.out.println("Start Music");
+    }
+
+    @Override
+    public void stopMusic() {
+        System.out.println("Stop Music");
+    }
+
+    @Override
+    public void changeMusic() {
+        System.out.println("Change Music");
+    }
+
+    @Override
+    public void openSunRoof() {
+        System.out.println("Opening Sun Roof");
+    }
+
+    @Override
+    public void openDoor() {
+        System.out.println("Opening Door");
+    }
+
+    @Override
+    public void start() {
+        System.out.println("Starting Engine");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Stop Engine");
+    }
+
+    @Override
+    public void engineType() {
+        System.out.println("Engine Type: Petrol");
+    }
+    
+}
