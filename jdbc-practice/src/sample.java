b3ebea043529d494926c7dc24cbeb5bff93bf2ea
Aathi <aathithirunav@gmail.com>
2026-06-24 22:14:23 +0530

    add sample Java program for database connectivity and user details retrieval

---
+++ jdbc-practice/src/sample.java
@@ -0,0 +1,39 @@
+package src;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class sample {
+    public static void main(String[] args) throws Exception {
+        String sql = "select * from User_Details";
+
+        String url = "jdbc:mysql://localhost:3306/first_db";
+        String user = "root";
+        String password = "Roronoa@Zoro"; 
+
+        Connection con = DriverManager.getConnection(url, user, password);
+
+        Statement st = con.createStatement();
+        ResultSet rs =  st.executeQuery(sql);
+        
+        
+        while(rs.next()) {
+            
+            int id = rs.getInt("Id");
+            String name = rs.getString("UserName");
+            int age = rs.getInt("Age");
+            String gender = rs.getString("Gender");
+            String skill = rs.getString("skill");
+            System.out.println("--------------------------------------");
+            System.out.print(" | " + id + " | " + name + " | " + age + " | " + gender + " | " + skill + " | ");
+            System.out.println();
+            
+        }
+        System.out.println("--------------------------------------");
+
+        con.close();
+
+    }
+}
