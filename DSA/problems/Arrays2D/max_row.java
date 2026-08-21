3aa0c69d8418ed15c43a344e0112fffa5f99fe52
Aathi <aathithirunav@gmail.com>
2026-08-21 19:19:28 +0530

    feat(dsa-2d-arrays): Implement solution to find max sum row in 2D array
    - Calculates the sum of elements for each row in a given 2D array.
    - Identifies and returns the index of the row that has the largest sum.

---
+++ DSA/problems/Arrays2D/max_row.java
@@ -0,0 +1,27 @@
+package Arrays.Arrays2D;
+
+public class max_row {
+    public static void main(String[] args) {
+        int[][] aa = {{1,2,3}, {4,5,6}, {1,1,1}};
+        
+        int max = 0;
+        int r=-1;
+        for(int i=0; i<3; i++){
+            int sum =0;
+            for(int j=0; j<3; j++){
+                sum+=aa[i][j];
+            } 
+            if(max<sum){
+                max=sum;
+                r=i;
+            }
+        } 
+
+        //  for(int i=0; i<2; i++){
+        //     for(int j=0; j<3; j++){
+        //         System.out.print(aa[i][j]);
+        //     } 
+             System.out.println(r);
+        // }
+    }
+}
