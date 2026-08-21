159d99c779b45cb848cba25f8bec06c0001b9f91
Aathi <aathithirunav@gmail.com>
2026-08-21 19:18:58 +0530

    feat(dsa-common-problems): Implement W pattern printer
    - Adds a Java program to print a 'W' shape pattern to the console.

---
+++ DSA/problems/common-problems/w_pattern.java
@@ -0,0 +1,51 @@
+public class w_pattern {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1; i<n; i++){
+                        // ............
+            for(int j=1; j<=i; j++){
+                if(i==1){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+
+            //...............
+
+            for(int j=i; j<=n; j++){
+                if(i+j==n+1){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+                
+            }
+            // ............
+             for(int j=1; j<=i; j++){
+                if(i==j){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print(" ");
+                }
+                
+            }
+            //...................
+
+            for(int j=i; j<=n; j++){
+                if(i==n){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+                
+            }
+            System.out.println();
+        }
+    }
+    
+}
