812401f6eb5ffa2fc193fda99115e7150312990b
Aathi <aathithirunav@gmail.com>
2026-08-21 19:15:17 +0530

    feat(dsa-common-problems): Implement Armstrong number check
    - Adds Java code to determine if a number is an Armstrong number.
    - The solution calculates the sum of the powers of its digits and compares it to the original number.

---
+++ DSA/problems/common-problems/armstrong.java
@@ -0,0 +1,31 @@
+//
+//
+//import java.util.Scanner;
+//class pattern{
+//    public static void main(String[] args){
+//        Scanner s= new Scanner(System.in);
+//        int n = s.nextInt();
+//        int count=0;
+//        int t=n ;
+//        int a=n;
+//        double num=0;
+//
+//        while(t!=0){
+//            count++;
+//            t/=10;
+//        }
+//
+//        while(a!=0){
+//           int d=a%10;
+//           num =num+ Math.pow(d,count);
+//           a/=10;
+//        }
+//        if(n == num){
+//                System.out.println("armstrong");
+//        }
+//        else{
+//             System.out.println("not");
+//        }
+//
+//    }
+//}
