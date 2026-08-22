79e4a41321df5c254bb96d96f73d608831cbfa2b
Aathi <aathithirunav@gmail.com>
2026-08-22 21:20:49 +0530

    refactor(dsa-2d-arrays): Rename 'max_row' problem and clean up code
    - Rename `max_row.java` to `FindRowWithMaxSum.java` for clearer problem identification.
    - Remove commented-out debugging code.

--- DSA/problems/Arrays2D/FindRowWithMaxSum.java
+++ DSA/problems/Arrays2D/FindRowWithMaxSum.java
@@ -17,11 +17,7 @@ public class max_row {
             }
         } 
 
-        //  for(int i=0; i<2; i++){
-        //     for(int j=0; j<3; j++){
-        //         System.out.print(aa[i][j]);
-        //     } 
-             System.out.println(r);
-        // }
+        System.out.println(r);
+
     }
 }
